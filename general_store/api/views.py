c91d1b2656c6922096dc292a18032e369444be4f
mleers <mleers4@gmail.com>
2021-05-10 12:08:16 -0700

    Add url patterns page

--- general_store/api/views.py
+++ general_store/api/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
 
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE", safe=False)
+    api_urls = {
+	    'List':'/item-list/',
+	    'Detail View':'/item-detail/<str:pk>/',
+	    'Create':'/item-create/',
+	    'Update':'/item-update/<str:pk>/',
+	    'Delete':'/item-delete/<str:pk>/',
+	    }
+    return Response(api_urls)
