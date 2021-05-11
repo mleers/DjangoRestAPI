48f57f13e5bf536ec22e2e21eada69ac22fcad52
mleers <mleers4@gmail.com>
2021-05-11 10:55:42 -0700

    Add basic item model

--- general_store/api/models.py
+++ general_store/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    in_stock = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
