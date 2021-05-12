6b0efed4b1545b7b53caa81341902130019cba1f
mleers <mleers4@gmail.com>
2021-05-12 12:30:42 -0700

    Add global Get for all items

--- general_store/api/urls.py
+++ general_store/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name="api-overview"),
+    path('item-list/', views.itemList, name="item-list"),
 ]
--- general_store/api/views.py
+++ general_store/api/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ItemSerializer
 
+from .models import Item
 # Create your views here.
 
 @api_view(['GET'])
@@ -16,3 +18,9 @@ def apiOverview(request):
 	    'Delete':'/item-delete/<str:pk>/',
 	    }
     return Response(api_urls)
+
+@api_view(['GET'])
+def itemList(request):
+	items = Item.objects.all()
+	serializer = ItemSerializer(items, many=True)
+	return Response(serializer.data) # query db, serialize data,return it in response
