137443e7fcde6b5aed55857606249a4dc3baadd7
mleers <mleers4@gmail.com>
2021-05-13 13:20:18 -0700

    Update model, add latest migration

--- general_store/api/models.py
+++ general_store/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=200)
+    price = models.DecimalField(default= 0.00, max_digits=10, decimal_places=2)
     in_stock = models.BooleanField(default=False)
 
     def __str__(self):
